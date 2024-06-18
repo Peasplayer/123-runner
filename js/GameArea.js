99f5717572e35cc1d4f020b57dfd4231dcd46dc6
feuerdoge <deniboz11@gmail.com>
2024-06-18 17:40:57 +0200

    Score auf Canvas
    Score auf Canvas

--- js/GameArea.js
+++ js/GameArea.js
@@ -1,15 +1,18 @@
 class GameArea {
     constructor() {
-        this.canvas = document.getElementsByTagName("canvas")[0] ?? document.createElement("canvas");
+        this.canvas = document.getElementsByTagName("canvas")[0] ?? document.createElement("canvas");       
         this.context = this.canvas.getContext("2d");
     }
-
     start() {
         this.canvas.width = 960;
-        this.canvas.height = 540;
+        this.canvas.height = 540;  
         document.body.append(this.canvas);
     }
-
+    drawScore(score) {
+        this.context.font = "15px Unispace";
+        this.context.fillStyle = "white";
+        this.context.fillText("Score: " + score, 10, 60);
+    }
     clear() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
--- js/MainGame.js
+++ js/MainGame.js
@@ -304,6 +304,7 @@ function updateGame() {
 
     // reset for dt
     lastUpdated = now;
+    this.gameArea.drawScore(this.score);
 }
 
 function sendPlayerToPortal(deltaTime) {
